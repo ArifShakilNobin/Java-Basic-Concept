c8824b5296b724231fe59332adc18f319f6caf1e
Arif.Shakil <arif.shakil@synesisit.com.bd>
2023-07-11 14:34:35 +0600

    Concept

--- Collections/ArrayListExample.java
+++ Collections/ArrayListExample.java
@@ -1,3 +1,50 @@
 package Collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ArrayListExample {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        //add elements
+        arrayList.add(5);
+        arrayList.add(1);
+        arrayList.add(2);
+
+        System.out.println(arrayList);
+
+        //get elements
+
+        int index = arrayList.get(0);
+        System.out.println(index);
+
+        // add el in between
+        arrayList.add(2,5);
+        System.out.println(arrayList);
+
+        //set element
+        arrayList.set(0,4);
+        System.out.println(arrayList);
+
+        //delete element
+        arrayList.remove(2);
+
+        //size
+        int size = arrayList.size();
+        System.out.println(size );
+
+        // loops
+        for (int i=0;i< arrayList.size(); i++){
+            System.out.print(arrayList.get(i));
+        }
+        System.out.println();
+
+        Collections.sort(arrayList);
+        System.out.println(arrayList);
+
+
+
+    }
 }
---
+++ Collections/HashMap Concept.md
@@ -0,0 +1,35 @@
+### Hash Map Concept
+
+1. Hash map is an associative array data structure.
+2. Store data in the form of <key, value> pair.
+3. Hashing HashCode() coming from the object class
+ Hashcode() ----> Hashing
+4. same data(key, value) can be store
+
+
+Map<String,Integer> marks = new HashMap<String,Integer>();
+
+marks.put("Naveen, 100); <br>
+marks.put("Tom, 200); <br>
+marks.put("Lisa, 300); <br>
+marks.put("Peter, 400); <br>
+marks.put("Robby, 500);
+
+
+| 0   |                   
+|-----|
+| 1   |  
+| 2   |  
+| 3   |  
+| 4   |  
+&#8593;
+
+
+
+| Key    | int hash | value | next node address          |               
+|--------|----------|-------|----------------------------|
+ | Naveen | 210678   | 100   | null (This time i use null |
+
+
+
+![img.png](img.png)
